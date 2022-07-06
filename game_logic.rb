ae18898d349008cad9602f59db40380cd5aa5b05
sulealtintas <sule@live.dk>
2022-07-06 20:57:35 +0600

    fix duplicate handling during code comparison

--- game_logic.rb
+++ game_logic.rb
@@ -1,20 +1,23 @@
 module GameLogic
   def compare(guess, code)
-    used_values = []
+    used_indexes = []
+    remaining_values = code.map(&:clone)
     exact_match = 0
     partial_match = 0
 
     guess.each_with_index do |n, i|
-      if guess[i] == code[i] and !used_values.include?(i)
+      if guess[i] == code[i]
         exact_match += 1
-        used_values.push(i)
+        remaining_values.delete_at(remaining_values.index(n))
+        used_indexes.push(i)
       end
     end
 
     guess.each_with_index do |n, i|
-      if code.include?(n) & !used_values.include?(i)
+      if remaining_values.include?(n) and !used_indexes.include?(i)
         partial_match += 1
-        used_values.push(i)
+        remaining_values.delete_at(remaining_values.index(n))
+        used_indexes.push(i)
       end
     end
 
